43c3919ff58251233b99934a3ab84e4f8f90bc42
quilicicf <quilicicf@gmail.com>
2022-02-01 14:02:54 +0100

    :construction: WIP

--- dist/mod.js
+++ dist/mod.js
@@ -108,101 +108,97 @@ function _createSuper(Derived) {
         return _possibleConstructorReturn(this, result);
     };
 }
-/**************
- * ANSI CODES *
- *************/ export var RESET_CODE = 0;
-export var ForegroundSimpleCode;
-(function(ForegroundSimpleCode) {
-    ForegroundSimpleCode[ForegroundSimpleCode["FG_Black"] = 30] = "FG_Black";
-    ForegroundSimpleCode[ForegroundSimpleCode["FG_Red"] = 31] = "FG_Red";
-    ForegroundSimpleCode[ForegroundSimpleCode["FG_Green"] = 32] = "FG_Green";
-    ForegroundSimpleCode[ForegroundSimpleCode["FG_Yellow"] = 33] = "FG_Yellow";
-    ForegroundSimpleCode[ForegroundSimpleCode["FG_Blue"] = 34] = "FG_Blue";
-    ForegroundSimpleCode[ForegroundSimpleCode["FG_Magenta"] = 35] = "FG_Magenta";
-    ForegroundSimpleCode[ForegroundSimpleCode["FG_Cyan"] = 36] = "FG_Cyan";
-    ForegroundSimpleCode[ForegroundSimpleCode["FG_White"] = 37] = "FG_White";
-    ForegroundSimpleCode[ForegroundSimpleCode["FG_Bright_Black"] = 90] = "FG_Bright_Black";
-    ForegroundSimpleCode[ForegroundSimpleCode["FG_Bright_Red"] = 91] = "FG_Bright_Red";
-    ForegroundSimpleCode[ForegroundSimpleCode["FG_Bright_Green"] = 92] = "FG_Bright_Green";
-    ForegroundSimpleCode[ForegroundSimpleCode["FG_Bright_Yellow"] = 93] = "FG_Bright_Yellow";
-    ForegroundSimpleCode[ForegroundSimpleCode["FG_Bright_Blue"] = 94] = "FG_Bright_Blue";
-    ForegroundSimpleCode[ForegroundSimpleCode["FG_Bright_Magenta"] = 95] = "FG_Bright_Magenta";
-    ForegroundSimpleCode[ForegroundSimpleCode["FG_Bright_Cyan"] = 96] = "FG_Bright_Cyan";
-    ForegroundSimpleCode[ForegroundSimpleCode["FG_Bright_White"] = 97] = "FG_Bright_White";
+var RESET_CODE = 0;
+var ForegroundSimpleCode;
+(function(ForegroundSimpleCode1) {
+    ForegroundSimpleCode1[ForegroundSimpleCode1["FG_Black"] = 30] = "FG_Black";
+    ForegroundSimpleCode1[ForegroundSimpleCode1["FG_Red"] = 31] = "FG_Red";
+    ForegroundSimpleCode1[ForegroundSimpleCode1["FG_Green"] = 32] = "FG_Green";
+    ForegroundSimpleCode1[ForegroundSimpleCode1["FG_Yellow"] = 33] = "FG_Yellow";
+    ForegroundSimpleCode1[ForegroundSimpleCode1["FG_Blue"] = 34] = "FG_Blue";
+    ForegroundSimpleCode1[ForegroundSimpleCode1["FG_Magenta"] = 35] = "FG_Magenta";
+    ForegroundSimpleCode1[ForegroundSimpleCode1["FG_Cyan"] = 36] = "FG_Cyan";
+    ForegroundSimpleCode1[ForegroundSimpleCode1["FG_White"] = 37] = "FG_White";
+    ForegroundSimpleCode1[ForegroundSimpleCode1["FG_Bright_Black"] = 90] = "FG_Bright_Black";
+    ForegroundSimpleCode1[ForegroundSimpleCode1["FG_Bright_Red"] = 91] = "FG_Bright_Red";
+    ForegroundSimpleCode1[ForegroundSimpleCode1["FG_Bright_Green"] = 92] = "FG_Bright_Green";
+    ForegroundSimpleCode1[ForegroundSimpleCode1["FG_Bright_Yellow"] = 93] = "FG_Bright_Yellow";
+    ForegroundSimpleCode1[ForegroundSimpleCode1["FG_Bright_Blue"] = 94] = "FG_Bright_Blue";
+    ForegroundSimpleCode1[ForegroundSimpleCode1["FG_Bright_Magenta"] = 95] = "FG_Bright_Magenta";
+    ForegroundSimpleCode1[ForegroundSimpleCode1["FG_Bright_Cyan"] = 96] = "FG_Bright_Cyan";
+    ForegroundSimpleCode1[ForegroundSimpleCode1["FG_Bright_White"] = 97] = "FG_Bright_White";
 })(ForegroundSimpleCode || (ForegroundSimpleCode = {}));
-export var BackgroundSimpleCode;
-(function(BackgroundSimpleCode) {
-    BackgroundSimpleCode[BackgroundSimpleCode["BG_Black"] = 40] = "BG_Black";
-    BackgroundSimpleCode[BackgroundSimpleCode["BG_Red"] = 41] = "BG_Red";
-    BackgroundSimpleCode[BackgroundSimpleCode["BG_Green"] = 42] = "BG_Green";
-    BackgroundSimpleCode[BackgroundSimpleCode["BG_Yellow"] = 43] = "BG_Yellow";
-    BackgroundSimpleCode[BackgroundSimpleCode["BG_Blue"] = 44] = "BG_Blue";
-    BackgroundSimpleCode[BackgroundSimpleCode["BG_Magenta"] = 45] = "BG_Magenta";
-    BackgroundSimpleCode[BackgroundSimpleCode["BG_Cyan"] = 46] = "BG_Cyan";
-    BackgroundSimpleCode[BackgroundSimpleCode["BG_White"] = 47] = "BG_White";
-    BackgroundSimpleCode[BackgroundSimpleCode["BG_Bright_Black"] = 100] = "BG_Bright_Black";
-    BackgroundSimpleCode[BackgroundSimpleCode["BG_Bright_Red"] = 101] = "BG_Bright_Red";
-    BackgroundSimpleCode[BackgroundSimpleCode["BG_Bright_Green"] = 102] = "BG_Bright_Green";
-    BackgroundSimpleCode[BackgroundSimpleCode["BG_Bright_Yellow"] = 103] = "BG_Bright_Yellow";
-    BackgroundSimpleCode[BackgroundSimpleCode["BG_Bright_Blue"] = 104] = "BG_Bright_Blue";
-    BackgroundSimpleCode[BackgroundSimpleCode["BG_Bright_Magenta"] = 105] = "BG_Bright_Magenta";
-    BackgroundSimpleCode[BackgroundSimpleCode["BG_Bright_Cyan"] = 106] = "BG_Bright_Cyan";
-    BackgroundSimpleCode[BackgroundSimpleCode["BG_Bright_White"] = 107] = "BG_Bright_White";
+var BackgroundSimpleCode;
+(function(BackgroundSimpleCode1) {
+    BackgroundSimpleCode1[BackgroundSimpleCode1["BG_Black"] = 40] = "BG_Black";
+    BackgroundSimpleCode1[BackgroundSimpleCode1["BG_Red"] = 41] = "BG_Red";
+    BackgroundSimpleCode1[BackgroundSimpleCode1["BG_Green"] = 42] = "BG_Green";
+    BackgroundSimpleCode1[BackgroundSimpleCode1["BG_Yellow"] = 43] = "BG_Yellow";
+    BackgroundSimpleCode1[BackgroundSimpleCode1["BG_Blue"] = 44] = "BG_Blue";
+    BackgroundSimpleCode1[BackgroundSimpleCode1["BG_Magenta"] = 45] = "BG_Magenta";
+    BackgroundSimpleCode1[BackgroundSimpleCode1["BG_Cyan"] = 46] = "BG_Cyan";
+    BackgroundSimpleCode1[BackgroundSimpleCode1["BG_White"] = 47] = "BG_White";
+    BackgroundSimpleCode1[BackgroundSimpleCode1["BG_Bright_Black"] = 100] = "BG_Bright_Black";
+    BackgroundSimpleCode1[BackgroundSimpleCode1["BG_Bright_Red"] = 101] = "BG_Bright_Red";
+    BackgroundSimpleCode1[BackgroundSimpleCode1["BG_Bright_Green"] = 102] = "BG_Bright_Green";
+    BackgroundSimpleCode1[BackgroundSimpleCode1["BG_Bright_Yellow"] = 103] = "BG_Bright_Yellow";
+    BackgroundSimpleCode1[BackgroundSimpleCode1["BG_Bright_Blue"] = 104] = "BG_Bright_Blue";
+    BackgroundSimpleCode1[BackgroundSimpleCode1["BG_Bright_Magenta"] = 105] = "BG_Bright_Magenta";
+    BackgroundSimpleCode1[BackgroundSimpleCode1["BG_Bright_Cyan"] = 106] = "BG_Bright_Cyan";
+    BackgroundSimpleCode1[BackgroundSimpleCode1["BG_Bright_White"] = 107] = "BG_Bright_White";
 })(BackgroundSimpleCode || (BackgroundSimpleCode = {}));
-export var DecorationCode;
-(function(DecorationCode) {
-    DecorationCode[DecorationCode["Bold"] = 1] = "Bold";
-    DecorationCode[DecorationCode["Dim"] = 2] = "Dim";
-    DecorationCode[DecorationCode["Italic"] = 3] = "Italic";
-    DecorationCode[DecorationCode["Underline"] = 4] = "Underline";
-    DecorationCode[DecorationCode["Blink_Slow"] = 5] = "Blink_Slow";
-    DecorationCode[DecorationCode["Blink_Fast"] = 6] = "Blink_Fast";
-    DecorationCode[DecorationCode["Reverse_Video"] = 7] = "Reverse_Video";
-    DecorationCode[DecorationCode["Conceal"] = 8] = "Conceal";
-    DecorationCode[DecorationCode["Crossed_Out"] = 9] = "Crossed_Out";
-    DecorationCode[DecorationCode["Primary"] = 10] = "Primary";
-    DecorationCode[DecorationCode["Alternative_Font_1"] = 11] = "Alternative_Font_1";
-    DecorationCode[DecorationCode["Alternative_Font_2"] = 12] = "Alternative_Font_2";
-    DecorationCode[DecorationCode["Alternative_Font_3"] = 13] = "Alternative_Font_3";
-    DecorationCode[DecorationCode["Alternative_Font_4"] = 14] = "Alternative_Font_4";
-    DecorationCode[DecorationCode["Alternative_Font_5"] = 15] = "Alternative_Font_5";
-    DecorationCode[DecorationCode["Alternative_Font_6"] = 16] = "Alternative_Font_6";
-    DecorationCode[DecorationCode["Alternative_Font_7"] = 17] = "Alternative_Font_7";
-    DecorationCode[DecorationCode["Alternative_Font_8"] = 18] = "Alternative_Font_8";
-    DecorationCode[DecorationCode["Alternative_Font_9"] = 19] = "Alternative_Font_9";
-    DecorationCode[DecorationCode["Fraktur"] = 20] = "Fraktur";
-    DecorationCode[DecorationCode["Double_Underline"] = 21] = "Double_Underline";
-    DecorationCode[DecorationCode["Normal"] = 22] = "Normal";
-    DecorationCode[DecorationCode["Italic_And_Fraktur_Off"] = 23] = "Italic_And_Fraktur_Off";
-    DecorationCode[DecorationCode["Underline_Off"] = 24] = "Underline_Off";
-    DecorationCode[DecorationCode["Blink_Off"] = 25] = "Blink_Off";
-    DecorationCode[DecorationCode["Proportional_Spacing"] = 26] = "Proportional_Spacing";
-    DecorationCode[DecorationCode["Reverse_And_Invert_Off"] = 27] = "Reverse_And_Invert_Off";
-    DecorationCode[DecorationCode["Reveal_And_Conceal_Off"] = 28] = "Reveal_And_Conceal_Off";
-    DecorationCode[DecorationCode["Crossed_Off"] = 29] = "Crossed_Off";
+var DecorationCode;
+(function(DecorationCode1) {
+    DecorationCode1[DecorationCode1["Bold"] = 1] = "Bold";
+    DecorationCode1[DecorationCode1["Dim"] = 2] = "Dim";
+    DecorationCode1[DecorationCode1["Italic"] = 3] = "Italic";
+    DecorationCode1[DecorationCode1["Underline"] = 4] = "Underline";
+    DecorationCode1[DecorationCode1["Blink_Slow"] = 5] = "Blink_Slow";
+    DecorationCode1[DecorationCode1["Blink_Fast"] = 6] = "Blink_Fast";
+    DecorationCode1[DecorationCode1["Reverse_Video"] = 7] = "Reverse_Video";
+    DecorationCode1[DecorationCode1["Conceal"] = 8] = "Conceal";
+    DecorationCode1[DecorationCode1["Crossed_Out"] = 9] = "Crossed_Out";
+    DecorationCode1[DecorationCode1["Primary"] = 10] = "Primary";
+    DecorationCode1[DecorationCode1["Alternative_Font_1"] = 11] = "Alternative_Font_1";
+    DecorationCode1[DecorationCode1["Alternative_Font_2"] = 12] = "Alternative_Font_2";
+    DecorationCode1[DecorationCode1["Alternative_Font_3"] = 13] = "Alternative_Font_3";
+    DecorationCode1[DecorationCode1["Alternative_Font_4"] = 14] = "Alternative_Font_4";
+    DecorationCode1[DecorationCode1["Alternative_Font_5"] = 15] = "Alternative_Font_5";
+    DecorationCode1[DecorationCode1["Alternative_Font_6"] = 16] = "Alternative_Font_6";
+    DecorationCode1[DecorationCode1["Alternative_Font_7"] = 17] = "Alternative_Font_7";
+    DecorationCode1[DecorationCode1["Alternative_Font_8"] = 18] = "Alternative_Font_8";
+    DecorationCode1[DecorationCode1["Alternative_Font_9"] = 19] = "Alternative_Font_9";
+    DecorationCode1[DecorationCode1["Fraktur"] = 20] = "Fraktur";
+    DecorationCode1[DecorationCode1["Double_Underline"] = 21] = "Double_Underline";
+    DecorationCode1[DecorationCode1["Normal"] = 22] = "Normal";
+    DecorationCode1[DecorationCode1["Italic_And_Fraktur_Off"] = 23] = "Italic_And_Fraktur_Off";
+    DecorationCode1[DecorationCode1["Underline_Off"] = 24] = "Underline_Off";
+    DecorationCode1[DecorationCode1["Blink_Off"] = 25] = "Blink_Off";
+    DecorationCode1[DecorationCode1["Proportional_Spacing"] = 26] = "Proportional_Spacing";
+    DecorationCode1[DecorationCode1["Reverse_And_Invert_Off"] = 27] = "Reverse_And_Invert_Off";
+    DecorationCode1[DecorationCode1["Reveal_And_Conceal_Off"] = 28] = "Reveal_And_Conceal_Off";
+    DecorationCode1[DecorationCode1["Crossed_Off"] = 29] = "Crossed_Off";
 })(DecorationCode || (DecorationCode = {}));
 function checkRgbRange(code) {
-    if (code < 0 || code > 255) {
-        throw Error("RGB codes should be between 0 & 255, got: ".concat(code));
-    }
+    if (code < 0 || code > 255) throw Error("RGB codes should be between 0 & 255, got: ".concat(code));
     return code;
 }
-export var RgbCode = function RgbCode(red, green, blue) {
+var RgbCode = function RgbCode1(red, green, blue) {
     "use strict";
-    _classCallCheck(this, RgbCode);
+    _classCallCheck(this, RgbCode1);
     this.red = checkRgbRange(red);
     this.green = checkRgbRange(green);
     this.blue = checkRgbRange(blue);
 };
-export var ForegroundRgbCode = /*#__PURE__*/ function(RgbCode) {
+var ForegroundRgbCode = /*#__PURE__*/ function(RgbCode2) {
     "use strict";
-    _inherits(ForegroundRgbCode, RgbCode);
-    var _super = _createSuper(ForegroundRgbCode);
-    function ForegroundRgbCode() {
-        _classCallCheck(this, ForegroundRgbCode);
+    _inherits(ForegroundRgbCode1, RgbCode2);
+    var _super = _createSuper(ForegroundRgbCode1);
+    function ForegroundRgbCode1() {
+        _classCallCheck(this, ForegroundRgbCode1);
         return _super.apply(this, arguments);
     }
-    _createClass(ForegroundRgbCode, [
+    _createClass(ForegroundRgbCode1, [
         {
             key: "toString",
             value: function toString() {
@@ -210,17 +206,17 @@ export var ForegroundRgbCode = /*#__PURE__*/ function(RgbCode) {
             }
         }
     ]);
-    return ForegroundRgbCode;
+    return ForegroundRgbCode1;
 }(RgbCode);
-export var BackgroundRgbCode = /*#__PURE__*/ function(RgbCode) {
+var BackgroundRgbCode = /*#__PURE__*/ function(RgbCode3) {
     "use strict";
-    _inherits(BackgroundRgbCode, RgbCode);
-    var _super = _createSuper(BackgroundRgbCode);
-    function BackgroundRgbCode() {
-        _classCallCheck(this, BackgroundRgbCode);
+    _inherits(BackgroundRgbCode1, RgbCode3);
+    var _super = _createSuper(BackgroundRgbCode1);
+    function BackgroundRgbCode1() {
+        _classCallCheck(this, BackgroundRgbCode1);
         return _super.apply(this, arguments);
     }
-    _createClass(BackgroundRgbCode, [
+    _createClass(BackgroundRgbCode1, [
         {
             key: "toString",
             value: function toString() {
@@ -228,9 +224,9 @@ export var BackgroundRgbCode = /*#__PURE__*/ function(RgbCode) {
             }
         }
     ]);
-    return BackgroundRgbCode;
+    return BackgroundRgbCode1;
 }(RgbCode);
-export function styleToAnsiCode(param) {
+function styleToAnsiCode(param) {
     var color = param.color, backgroundColor = param.backgroundColor, decoration = param.decoration, shouldResetFirst = arguments.length > 1 && arguments[1] !== void 0 ? arguments[1] : false;
     var codes = _toConsumableArray(shouldResetFirst ? [
         RESET_CODE
@@ -243,22 +239,20 @@ export function styleToAnsiCode(param) {
     });
     return codes.length === 0 ? '' : "\x1b[".concat(codes.join(';'), "m");
 }
-export var StyleMode;
-(function(StyleMode) {
-    StyleMode[StyleMode["STYLE"] = 0] = "STYLE";
-    StyleMode[StyleMode["NO_STYLE"] = 1] = "NO_STYLE";
+var StyleMode;
+(function(StyleMode1) {
+    StyleMode1[StyleMode1["STYLE"] = 0] = "STYLE";
+    StyleMode1[StyleMode1["NO_STYLE"] = 1] = "NO_STYLE";
 })(StyleMode || (StyleMode = {}));
 function areStylesEqual(style1, style2) {
     return style1.color === style2.color && style1.backgroundColor === style2.backgroundColor && style1.decoration === style2.decoration;
 }
 function computeNextFragment(string, lastStyle, styleToApply) {
-    if (!styleToApply || areStylesEqual(styleToApply, lastStyle)) {
-        return {
-            string: string,
-            shouldResetFirst: false,
-            style: undefined
-        };
-    }
+    if (!styleToApply || areStylesEqual(styleToApply, lastStyle)) return {
+        string: string,
+        shouldResetFirst: false,
+        style: undefined
+    };
     var shouldRemoveOldColor = !!lastStyle.color && !styleToApply.color;
     var shouldRemoveOldBackgroundColor = !!lastStyle.backgroundColor && !styleToApply.backgroundColor;
     var shouldRemoveOldDecoration = !!lastStyle.decoration && styleToApply.decoration !== lastStyle.decoration;
@@ -274,9 +268,7 @@ function computeNextFragment(string, lastStyle, styleToApply) {
     };
 }
 function computeNextCode(lastCode, newCode, hasReset) {
-    if (newCode) {
-        return newCode;
-    }
+    if (newCode) return newCode;
     return hasReset || !lastCode ? undefined : lastCode;
 }
 function printFragmentStyle(fragment) {
@@ -293,11 +285,11 @@ function getSpecificStyle(index, styles) {
     var halfIndex = ~~(index / 2);
     return isEven ? styles === null || styles === void 0 ? void 0 : (ref = styles.edges) === null || ref === void 0 ? void 0 : ref[halfIndex] : styles === null || styles === void 0 ? void 0 : (ref1 = styles.nodes) === null || ref1 === void 0 ? void 0 : ref1[halfIndex];
 }
-export function stoyleString(input, style) {
+function stoyleString(input, style) {
     var styleMode = arguments.length > 2 && arguments[2] !== void 0 ? arguments[2] : StyleMode.STYLE;
     return styleMode === StyleMode.NO_STYLE ? input : styleToAnsiCode(style) + input + styleToAnsiCode({}, true);
 }
-export function stoyleGlobal(edgesAsTemplateStringArray) {
+function stoyleGlobal(edgesAsTemplateStringArray) {
     for(var _len = arguments.length, nodes = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++){
         nodes[_key - 1] = arguments[_key];
     }
@@ -310,30 +302,23 @@ export function stoyleGlobal(edgesAsTemplateStringArray) {
         return stoyleString(wholeString, style, styleMode);
     };
 }
-export function stoyle(edgesAsTemplateStringArray) {
+function stoyle(edgesAsTemplateStringArray) {
     for(var _len = arguments.length, nodes = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++){
         nodes[_key - 1] = arguments[_key];
     }
     var edges = _toConsumableArray(edgesAsTemplateStringArray);
     return function(styles) {
         var styleMode = arguments.length > 1 && arguments[1] !== void 0 ? arguments[1] : StyleMode.STYLE;
-        var ref8, ref2;
-        if (styles.edges && edges.length !== ((ref8 = styles.edges) === null || ref8 === void 0 ? void 0 : ref8.length)) {
-            var ref3;
-            throw Error("I found ".concat(edges.length, " edge(s), but ").concat((ref3 = styles.edges) === null || ref3 === void 0 ? void 0 : ref3.length, " edge style(s)!"));
-        }
-        if (styles.nodes && nodes.length !== ((ref2 = styles.nodes) === null || ref2 === void 0 ? void 0 : ref2.length)) {
-            var ref4;
-            throw Error("I found ".concat(nodes.length, " node(s), but ").concat((ref4 = styles.nodes) === null || ref4 === void 0 ? void 0 : ref4.length, " node style(s)!"));
-        }
+        var ref8, ref2, ref3, ref4;
+        if (styles.edges && edges.length !== ((ref8 = styles.edges) === null || ref8 === void 0 ? void 0 : ref8.length)) throw Error("I found ".concat(edges.length, " edge(s), but ").concat((ref2 = styles.edges) === null || ref2 === void 0 ? void 0 : ref2.length, " edge style(s)!"));
+        if (styles.nodes && nodes.length !== ((ref3 = styles.nodes) === null || ref3 === void 0 ? void 0 : ref3.length)) throw Error("I found ".concat(nodes.length, " node(s), but ").concat((ref4 = styles.nodes) === null || ref4 === void 0 ? void 0 : ref4.length, " node style(s)!"));
         var ref5;
         var globalStyle = (ref5 = styles === null || styles === void 0 ? void 0 : styles.global) !== null && ref5 !== void 0 ? ref5 : {}; // Defaults to no style
         var accumulator = new Array(edges.length + nodes.length).fill(null).reduce(function(seed, whatever, index) {
             var ref, ref6, ref7;
             var string = getString(index, edges, nodes);
-            if (!string) {
-                return seed;
-            } // Nothing to write for empty strings
+            if (!string) return seed;
+             // Nothing to write for empty strings
             var specificStyle = getSpecificStyle(index, styles);
             var styleToApply = specificStyle !== null && specificStyle !== void 0 ? specificStyle : globalStyle; // Specific styles supersede global style
             var fragment = styleMode === StyleMode.NO_STYLE ? {
@@ -364,5 +349,17 @@ export function stoyle(edgesAsTemplateStringArray) {
         }).join('');
     };
 }
-
-//# sourceMappingURL=mod.js.map
+/**************
+ * ANSI CODES *
+ *************/ export { RESET_CODE as RESET_CODE };
+export { ForegroundSimpleCode as ForegroundSimpleCode };
+export { BackgroundSimpleCode as BackgroundSimpleCode };
+export { DecorationCode as DecorationCode };
+export { RgbCode as RgbCode };
+export { ForegroundRgbCode as ForegroundRgbCode };
+export { BackgroundRgbCode as BackgroundRgbCode };
+export { styleToAnsiCode as styleToAnsiCode };
+export { StyleMode as StyleMode };
+export { stoyleString as stoyleString };
+export { stoyleGlobal as stoyleGlobal };
+export { stoyle as stoyle };
